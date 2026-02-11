c0cda3952fcbf67a3b7accc071faeff092bcbf49
tamhid-mahmod <tamhidmahmod94@gmail.com>
2026-02-11 17:04:43 +0600

    trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { caller } from "@/trpc/server";
 
-export default function Home() {
+export default async function Page() {
+  const users = await caller.getUsers();
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button>Click Me!</Button>
+      {JSON.stringify(users)}
     </div>
   );
 }
