396f09fc55d16cd6502ec9d25d4e7d3abdbb69a0
tamhid-mahmod <tamhidmahmod94@gmail.com>
2026-02-21 15:33:05 +0600

    ai-providers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,6 +4,13 @@ import { createTRPCRouter, protectedProcedure } from "../init";
 import { inngest } from "@/inngest/client";
 
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    });
+
+    return { success: true, message: "Job queued" };
+  }),
   getWorkflows: protectedProcedure.query(() => {
     return prisma.workflow.findMany();
   }),
